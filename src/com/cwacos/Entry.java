011f206a4c836442fe962dc9a10654753b0a6415
Michael Leonard <mcleonar@uncg.edu>
2021-02-25 20:45:24 -0500

    This class will call and store the Intraday JSON for the given stock name. Entry is a supporting class whose objects are used by APIProgram.

---
+++ src/com/cwacos/Entry.java
@@ -0,0 +1,27 @@
+public class Entry {
+
+    double open, close, low, high;
+    int volume;
+
+    public Entry(){
+    }
+
+    public Entry (double _open, double _close, double _low, double _high, int _volume){
+        this.open = _open;
+        this.close = _close;
+        this.low = _low;
+        this.high = _high;
+        this.volume = _volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "open=" + open +
+                ", close=" + close +
+                ", low=" + low +
+                ", high=" + high +
+                ", volume=" + volume +
+                '}';
+    }
+}

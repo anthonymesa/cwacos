bba8b78eaff2d38ab27ecb7f72a6694c2b682dcc
Michael Leonard <mcleonar@uncg.edu>
2021-03-30 14:20:25 -0400

    Added date-time String to Entry objects

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -2,16 +2,18 @@ public class Entry {
 
     double open, close, low, high;
     int volume;
+    String dateTime;
 
     public Entry(){
     }
 
-    public Entry (double _open, double _close, double _low, double _high, int _volume){
+    public Entry (double _open, double _close, double _low, double _high, int _volume, String _dateTime){
         this.open = _open;
         this.close = _close;
         this.low = _low;
         this.high = _high;
         this.volume = _volume;
+        this.dateTime = _dateTime;
     }
 
     @Override
@@ -22,6 +24,7 @@ public class Entry {
                 ", low=" + low +
                 ", high=" + high +
                 ", volume=" + volume +
+                ", dateTime=" + dateTime +
                 '}';
     }
 }

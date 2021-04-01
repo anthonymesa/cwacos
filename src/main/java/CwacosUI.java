455b473e138ece7215ceb2b72a18388e8351f026
bugg86 <jdfink@uncg.edu>
2021-03-31 20:10:08 -0400

    Add the function to give the dialog box button functionality. Also changed the beginUI function to startUI and made it reference CwacosView.

--- src/main/java/CwacosUI.java
+++ src/main/java/CwacosUI.java
@@ -1,25 +1,4 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 public class CwacosUI  {
 
     public CwacosUI(){}
-
-    public Button testBtnFunction(Button btn) {
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        return btn;
-    }
 }

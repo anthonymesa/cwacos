b2910cf6cd681e2af1df03b7f1cb74a274753d38
aDjido <78118418+aDjido@users.noreply.github.com>
2021-04-29 23:55:45 -0400

    Basic lineChart population, wasnt able to test so may need changes

--- src/main/java/com/cwacos/CwacosView.java
+++ src/main/java/com/cwacos/CwacosView.java
@@ -1145,7 +1145,7 @@ public class CwacosView extends Application {
         XYChart.Series<Number,Number> series = new XYChart.Series<>();
         series.setName("Cwacos");
 
-        //populating line chart
+
         ArrayList<Entry> entries = CwacosData.getActiveEntryList();
 
         if(entries!= null){
